a1b983851ffbedcd58f702e1e5333ac9949f627b
gabriella.silva <gabriella.silva@DEV-PERLINK10.perlink.net>
2019-09-17 22:07:21 -0300

    removidos codigos comentados em criarPDF

--- src/dao/CriarPDF.java
+++ src/dao/CriarPDF.java
@@ -31,12 +31,10 @@ import model.requerimento.Requerimento;
 public class CriarPDF {
     private static Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
             Font.BOLD);
-    //private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 12,
-    //        Font.NORMAL, BaseColor.RED);
+
     private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16,
             Font.BOLD);
-   //private static  Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12,
-   //         Font.BOLD);
+
     public CriarPDF(Requerimento requerimento){
     String FILE = "../Relatorios/" + requerimento.getNumeroProtocolo() +".pdf";
      try {
